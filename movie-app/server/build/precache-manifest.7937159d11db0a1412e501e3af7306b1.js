f0987247b69775c76e2b896b676f68c22f7330d5
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2022-04-11 13:06:59 -0500

    movies-app

---
+++ movie-app/server/build/precache-manifest.7937159d11db0a1412e501e3af7306b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594e8e58e05b32be879550a9eaab04d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d3c3f59847066c4cd2",
+    "url": "/static/css/main.6b3f7275.chunk.css"
+  },
+  {
+    "revision": "33750a329a4c0af34fab",
+    "url": "/static/js/2.3e9db537.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e9db537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d3c3f59847066c4cd2",
+    "url": "/static/js/main.14f0ba86.chunk.js"
+  },
+  {
+    "revision": "9dbba9571719b5740e25",
+    "url": "/static/js/runtime-main.2bcd3a86.js"
+  }
+]);
